a81a9bd80b6d5e6ba5a6f1c564f35c7b16282c10
Gideon <156165263+kidgideon@users.noreply.github.com>
2024-04-26 06:30:34 -0930

    Add files via upload
    initial uploads

---
+++ index.js
@@ -0,0 +1,361 @@
+  
+   let loopingFFunctionOne;
+  
+  function scrollToSetcion(sectionClass) {
+    const section = document.querySelector(sectionClass);
+   
+     if (section) {
+        section.scrollIntoView({
+            behaviour: "smooth"
+            
+    })
+     } else {
+        console.log("error")
+     }
+ 
+   } 
+
+   const popup = () => {
+      const blurLayer = document.querySelector(".blur-layer");
+       const elem = document.querySelector(".popup");
+        if (elem.classList.contains("show")) {
+          elem.classList.remove("show")
+          blurLayer.style.visibility = "hidden";
+        } else {
+          elem.classList.add("show")
+          blurLayer.style.visibility = "visible";
+        }
+    }
+
+   const loopingFunctionthree = () => {
+      const platform = document.querySelector(".job-desc");
+      const span = document.querySelector(".la-span");
+  
+setTimeout(() => {
+span.innerHTML = 'S';
+setTimeout(() => {
+span.innerHTML += 'o';
+setTimeout(() => {
+span.innerHTML += 'f';
+setTimeout(() => {
+span.innerHTML += 't';
+setTimeout(() => {
+span.innerHTML += 'w';
+setTimeout(() => {
+span.innerHTML += 'a';
+setTimeout(() => {
+span.innerHTML += 'r';
+setTimeout(() => {
+span.innerHTML += 'e';
+setTimeout(() => {
+platform.innerHTML += 'D';
+setTimeout(() => {
+platform.innerHTML += 'e';
+setTimeout(() => {
+platform.innerHTML += 'v';
+setTimeout(() => {
+platform.innerHTML += 'e';
+setTimeout(() => {
+platform.innerHTML += 'l';
+setTimeout(() => {
+platform.innerHTML += 'o';
+setTimeout(() => {
+platform.innerHTML += 'p';
+setTimeout(() => {
+platform.innerHTML += 'e';
+setTimeout(() => {
+platform.innerHTML += 'r';
+
+setTimeout(() => {
+platform.innerHTML = `<span>Software</span> Developer`;
+setTimeout(() => {
+platform.innerHTML = `<span>Software</span> Develop`;
+setTimeout(() => {
+  platform.innerHTML = `<span>Software</span> Develo`;
+  setTimeout(() => {
+      platform.innerHTML = `<span>Software</span> Devel`;
+      setTimeout(() => {
+          platform.innerHTML = `<span>Software</span> Deve`;
+          setTimeout(() => {
+              platform.innerHTML = `<span>Software</span> Dev`;
+              setTimeout(() => {
+                  platform.innerHTML = `<span>Software</span> Dev`;
+                  setTimeout(() => {
+                      platform.innerHTML = `<span>Software</span> De`;
+                      setTimeout(() => {
+                          platform.innerHTML = `<span>Software</span> D`;
+                          setTimeout(() => {
+                              platform.innerHTML = `<span>Software</span>`;
+                              setTimeout(() => {
+                                  platform.innerHTML = `<span>Softwar</span>`;
+                                  setTimeout(() => {
+                                      platform.innerHTML = `<span>Softwa</span>`;
+                                      setTimeout(() => {
+                                          platform.innerHTML = `<span>Softw</span>`;
+                                          setTimeout(() => {
+                                              platform.innerHTML = `<span>Soft</span>`;
+                                              setTimeout(() => {
+                                                  platform.innerHTML = `<span>Sof</span>`;
+                                                  setTimeout(() => {
+                                                      platform.innerHTML = `<span>So</span>`;
+                                                      setTimeout(() => {
+                                                          platform.innerHTML = `<span>S</span>`;
+                                                          setTimeout(() => {
+                                                              platform.innerHTML = `<span class="la-span"></span>`;
+                                                              loopingFFunctionOne();
+                                                          }, 300);
+                                                      }, 300);
+                                                  }, 300);
+                                              }, 300);
+                                          }, 300);
+                                      }, 300);
+                                  }, 300);
+                              }, 300);
+                          }, 300);
+                      }, 300);
+                  }, 300);
+              }, 300);
+          }, 300);
+      }, 300);
+  }, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+}, 300);
+};
+  
+
+
+    const loopingFFunctionTwo = () => {
+      const platform = document.querySelector(".job-desc")
+       platform.innerHTML = `<span class="la-span"></span>`;
+      const span = document.querySelector(".la-span")
+
+          setTimeout(() => {
+        span.innerHTML = `W`
+         setTimeout(() => {
+          span.innerHTML += 'e'
+          setTimeout(() => {
+            span.innerHTML += 'b'
+            setTimeout(() => {
+              platform.innerHTML += "D"
+               setTimeout(() => {
+                platform.innerHTML += "e"
+                setTimeout(() => {
+                  platform.innerHTML += "v"
+                  setTimeout(() => {
+                    platform.innerHTML += "e"
+                    setTimeout(() => {
+                      platform.innerHTML += "l"
+                      setTimeout(() => {
+                        platform.innerHTML += "o"
+                        setTimeout(() => {
+                          platform.innerHTML += "p"
+                          setTimeout(() => {
+                            platform.innerHTML += "e"
+                            setTimeout(() => {
+                              platform.innerHTML += "r"
+  
+  setTimeout(() => {
+    platform.innerHTML = "<span>Web</span> Developer";
+    setTimeout(() => {
+      platform.innerHTML = "<span>Web</span> Develope";
+      setTimeout(() => {
+        platform.innerHTML =  "<span>Web</span> Develop";
+        setTimeout(() => {
+          platform.innerHTML = "<span>Web</span> Develo";
+          setTimeout(() => {
+            platform.innerHTML = "<span>Web</span> Devel"
+            setTimeout(() => {
+platform.innerHTML = "<span>Web</span> Deve"
+ setTimeout(() => {
+           platform.innerHTML = "<span>Web</span> Dev"
+            setTimeout(() => {
+              platform.innerHTML = "<span>Web</span> De"
+              setTimeout(() => {
+                platform.innerHTML = "<span>Web</span> D"
+                setTimeout(() => {
+                 platform.innerHTML = "<span>Web</span>"
+                 setTimeout(() => {
+                  platform.innerHTML = "<span>We</span>"
+                  setTimeout(() => {
+                   platform.innerHTML = "<span>W</span>"
+                   setTimeout(() => {
+                    platform.innerHTML = `<span class="la-span"></span>`
+                    loopingFunctionthree()
+                   }, 300)
+                  }, 300)
+                 }, 300)
+                }, 300)
+              }, 300)
+            }, 300)
+ }, 300)
+            }, 300)
+          }, 300)
+        }, 300)
+      }, 300)
+    }, 300)
+  }, 300)
+
+                            }, 300)
+                          }, 300)
+                        }, 300)
+                      }, 300)
+                    }, 300)
+                  }, 300)
+                }, 300)
+               }, 300)
+             },300)
+          }, 300)
+         }, 300)
+          }, 300)
+    }
+  
+
+      loopingFFunctionOne = () => {
+    const platform = document.querySelector(".job-desc")
+    const span = document.querySelector(".la-span")
+
+     setTimeout(() => {
+    span.innerHTML = 'F'
+       setTimeout(() => {
+      span.innerHTML += 'r'
+         setTimeout(() => {
+        span.innerHTML += 'o'
+           setTimeout(() => {
+          span.innerHTML += "n"
+             setTimeout(() => {
+            span.innerHTML += "t"
+              setTimeout(() => {
+              span.innerHTML += "e"
+                 setTimeout(() => {
+                span.innerHTML += "n"
+                  setTimeout(() => {
+                 span.innerHTML += "d"
+                       setTimeout(() => {
+                        platform.innerHTML += "D"
+                         setTimeout(() => {
+                          platform.innerHTML += "e"
+                          setTimeout(() => {
+                            platform.innerHTML += "v"
+                            setTimeout(() => {
+                              platform.innerHTML += "e"
+                              setTimeout(() => {
+                                platform.innerHTML += "l"
+                                setTimeout(() => {
+                                  platform.innerHTML += "o"
+                                  setTimeout(() => {
+                                    platform.innerHTML += "p"
+                                    setTimeout(() => {
+                                      platform.innerHTML += "e"
+                                      setTimeout(() => {
+                                        platform.innerHTML += "r"
+
+      setTimeout(() => {
+        platform.innerHTML = `<span>Frontend</span> Developer`
+         setTimeout(() => {
+          platform.innerHTML = `<span>Frontend</span> Develope`
+          setTimeout(() => {
+            platform.innerHTML = `<span>Frontend</span> Develop`
+            setTimeout(() => {
+              platform.innerHTML = `<span>Frontend</span> Develo`
+               setTimeout(() => {
+                platform.innerHTML = `<span>Frontend</span> Devel`
+                setTimeout(() => {
+                  platform.innerHTML = `<span>Frontend</span> Deve`
+                  setTimeout(() => {
+                    platform.innerHTML = `<span>Frontend</span> Dev`
+                    setTimeout(() => {
+                      platform.innerHTML = `<span>Frontend</span> Dev`
+                      setTimeout(() => {
+                        platform.innerHTML = `<span>Frontend</span> De`
+                        setTimeout(() => {
+                          platform.innerHTML = `<span>Frontend</span> D`
+                          setTimeout(() => {
+                            platform.innerHTML = `<span>Frontend</span>`   
+                            setTimeout(() => {
+                              platform.innerHTML = `<span>Fronten</span>` 
+                              setTimeout(() => {
+                                platform.innerHTML =  `<span>Fronte</span>`
+                                setTimeout(() => {
+                                  platform.innerHTML =  `<span>Front</span>`
+                                  setTimeout(() => {
+                                    platform.innerHTML =  `<span>Fron</span>`
+                                    setTimeout(() => {
+                                      platform.innerHTML = `<span>Fro</span>`
+                                      setTimeout(() => {
+                                        platform.innerHTML = `<span>Fr</span>`
+                                        setTimeout(() => {
+                                          platform.innerHTML = `<span>F</span>`
+                                          setTimeout(() => {
+                                            platform.innerHTML = `<span class="la-span"></span>`
+                                            loopingFFunctionTwo()
+                                          }, 300)
+                                        }, 300)
+                                      }, 300)
+                                    }, 300)
+                                  }, 300)
+                                }, 300)
+                              }, 300)
+                            }, 300)
+                          }, 300)
+                        }, 300)
+                      }, 300)
+                    }, 300)
+                  }, 300)
+                }, 300)
+               }, 300)
+            }, 300)
+          }, 300)
+         }, 300)
+      }, 300) 
+                                      }, 300)
+                                    }, 300)
+                                  }, 300)
+                                }, 300)
+                              }, 300)
+                            }, 300)
+                          }, 300)
+                         }, 300)
+                       },300)
+                  },300)
+                 },300)
+              },300)
+             },300)
+           },300)
+         },300)
+       },300)
+     }, 300)
+     }
+
+     const learnMore = () => {
+      const blurLayer = document.querySelector(".blur-layer");
+       const secondPopup = document.querySelector(".popup2")
+        if (secondPopup.classList.contains("show2")) {
+          secondPopup.classList.remove("show2")
+          blurLayer.style.visibility = "hidden";
+        } else {
+          secondPopup.classList.add("show2")
+          blurLayer.style.visibility = "visible";
+        }
+     }
+    
+      loopingFFunctionOne()
+
+       
